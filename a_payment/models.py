b7c2ac55d0e03eb0a28f06d4b4929c048d58a62e
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-10-22 13:09:15 -0300

    update payment-model

--- a_payment/models.py
+++ a_payment/models.py
@@ -1,49 +1,71 @@
 from django.db import models
 from django.contrib.auth.models import User
 from a_store.models import Product
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver 
+import datetime
 
 
 # Create your models here.
 
 
 class ShippingAddress(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    shipping_full_name = models.CharField(max_length=20)
-    shipping_email = models.EmailField(max_length=40)
-    shipping_address = models.CharField(max_length=255)
-    shipping_street = models.CharField(max_length=255, blank=True, null=True)  # Opcional
-    shipping_city = models.CharField(max_length=100)
-    shipping_zipcode = models.CharField(max_length=20)
-    shipping_country = models.CharField(max_length=100)
-    
-    
-    def __str__(self):
-        return f'Shipping Address - {str(self.id)}'
-    
-    class Meta:
-        verbose_name_plural = "Shipping Address"
-        
-        
-        
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	shipping_full_name = models.CharField(max_length=255)
+	shipping_email = models.EmailField(max_length=255)
+	shipping_address = models.CharField(max_length=255)
+	shipping_street = models.CharField(max_length=255, blank=True, null=True)
+	shipping_city = models.CharField(max_length=255)
+	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
+	shipping_country = models.CharField(max_length=255)
+
+
+	# Don't pluralize address
+	class Meta:
+		verbose_name_plural = "Shipping Address"
+
+	def __str__(self):
+		return f'Shipping Address - {str(self.id)}'
+
+
+
+# Create Order Model
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    full_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=250)
-    shipping_address = models.TextField(max_length=500)
-    amount_paid = models.DecimalField(max_digits=100, decimal_places=0)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'Order - {str(self.id)}'
-    
-    
+	# Foreign Key
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	full_name = models.CharField(max_length=250)
+	email = models.EmailField(max_length=250)
+	shipping_address = models.TextField(max_length=15000)
+	amount_paid = models.DecimalField(max_digits=1000, decimal_places=2)
+	date_ordered = models.DateTimeField(auto_now_add=True)	
+	shipped = models.BooleanField(default=False)
+	date_shipped = models.DateTimeField(blank=True, null=True)
+	
+	def __str__(self):
+		return f'Order - {str(self.id)}'
+
+
+# Auto Add shipping Date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+	if instance.pk:
+		now = datetime.datetime.now()
+		obj = sender._default_manager.get(pk=instance.pk)
+		if instance.shipped and not obj.shipped:
+			instance.date_shipped = now
+
+
+
+# Create Order Items Model
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    quantity = models.PositiveBigIntegerField(default=1)
-    price = models.DecimalField(max_digits=100, decimal_places=0)
-    
-    
-    def __str__(self):
-        return f'Order Item - {str(self.id)}'
+	# Foreign Keys
+	order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+	quantity = models.PositiveBigIntegerField(default=1)
+	price = models.DecimalField(max_digits=1000, decimal_places=2)
+
+
+	def __str__(self):
+		return f'Order Item - {str(self.id)}'
